6ddb423768f54ee6ac64f12b26d4ca88a242878b
Ngwere <ngwerecliford@gmail.com>
2022-12-10 02:52:33 -0800

    hello world in c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+/** main - Print "Programming is like 
+ *building a multilingual puzzle
+ *Return: Zero if successful
+ */
+
+int main (void)
+{
+  printf("\n Programming is like building a multilingual puzzle");
+  return (0);
+}
