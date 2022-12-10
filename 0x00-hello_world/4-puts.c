8c55b01980195bf875a9b69b05952f41acc693c6
Ngwere <ngwerecliford@gmail.com>
2022-12-10 11:50:24 -0800

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,12 @@
 #include<stdio.h>
 
-
 /**
  *main - entry point
  *Return: Zero if successful
  */
+
 int main(void)
 {
-        puts("\"Programming is like building a multilingual puzzle\n"); 
-        return (0);
-
-	 
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return (0);
 }
