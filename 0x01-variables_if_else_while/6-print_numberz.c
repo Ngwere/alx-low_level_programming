8f93fabbd36ba796c7ba2588a161a0c6b3d50648
Ngwere <ngwerecliford@gmail.com>
2022-12-11 08:08:04 -0800

    0 - 9 using putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - print 0 - 9 using putchar
+ * Return: 0 for success
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n <= 10; n++)
+		putchar(n);
+	putchar(n);
+	return (0);
+}
