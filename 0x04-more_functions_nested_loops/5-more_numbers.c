1e88ae805e99b28f1743468d772f94c320b9c73d
Ngwere <ngwerecliford@gmail.com>
2022-12-16 12:21:01 -0800

    10x 0 - 14

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * more_numbers - 10x 0 - 14
+ */
+void more_numbers(void)
+{
+	int n;
+	int k;
+
+	for (k = 0; k < 10; k++)
+	{
+		for (n = 0; n <= 14; n++)
+		{
+			_putchar('0' + n);
+		}
+		_putchar('0' + n);
+	}
+}
+
