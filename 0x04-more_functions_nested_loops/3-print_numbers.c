3703942d065bb51ea15ed27697862759b370b9c3
Ngwere <ngwerecliford@gmail.com>
2022-12-15 21:40:38 -0800

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#incude "main.h"
+
+/**
+ * print_numbers - print from 0 - 9
+ * Retrun: 0
+ */
+void print_numbers(void)
+{
+	int n;
+
+	for (n = 0; n <= 10; n++)
+		_putchar(n);
+	_putchart('\n');
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -4,5 +4,6 @@
 int mul(int a, int b);
 int _isupper(int c);
 int _isdigit(int c);
+void print_numbers(void);
 
 #endif
