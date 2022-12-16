ea7ff269b58c411da135f8ecedc8c41b9c46cf3e
Ngwere <ngwerecliford@gmail.com>
2022-12-16 13:41:34 -0800

    7-print_diagonal.c

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_diagonal - diagonal art
+ * @n: the number of lines
+ * Return: void
+ */
+void print_diagonal(int n)
+{
+	int i;
+
+	for (i = 0; i < n - 1; i++)
+	{
+		_putchar(' ');
+	}
+	_putchar('\\');
+	_putchar('\n');
+}
