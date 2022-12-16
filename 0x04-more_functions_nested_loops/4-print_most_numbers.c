60969cecfec68baaf2c302cfb021341c9cfcf780
Ngwere <ngwerecliford@gmail.com>
2022-12-16 12:01:56 -0800

    0 - 9 except 2 and 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - 0 to 9 except 2 and 4
+ */
+void print_most_numbers(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+	{
+		if (n != 2 || n != 4)
+		{
+			_printf('0' + n);
+		}
+	}
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -6,5 +6,6 @@ int _isupper(int c);
 int _isdigit(int c);
 void print_numbers(void);
 int _putchar(int c);
+void print_most_numbers(void);
 
 #endif
