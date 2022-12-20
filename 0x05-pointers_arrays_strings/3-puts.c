118eb25fb2b3c8f2856f8e6ef96464f06c158b00
Ngwere <ngwerecliford@gmail.com>
2022-12-19 23:33:09 -0800

    Added 3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _puts - print string to stdout
+ *@str: string to print
+ * Return: void
+ */
+void _puts(char *str)
+{
+	int c;
+
+	for (c = 0; *str[c] != '\0'; c++)
+		_putchar(*str[c]);
+}
