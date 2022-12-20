ef0462371d383eec1545846e0ebf30ac8ce16a7c
Ngwere <ngwerecliford@gmail.com>
2022-12-19 17:44:36 -0800

    1-swap.c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+* reset_to_98 - change variable
+* @a: pointer to the variable to be changed
+* @b: second
+* Return: void
+*/
+void swap_int(int *a, int *b)
+{
+*a = *b;
+*b = *a;
+}
