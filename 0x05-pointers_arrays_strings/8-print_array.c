fe806338dd58020c739c09e24b8e93a76147c1f7
Ngwere <ngwerecliford@gmail.com>
2022-12-20 03:19:13 -0800

    added 8-print_array.c

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_array - print the first n of array
+ *@a: array
+ *@n: number to stop at
+ * Return: void
+ */
+void print_array(int *a, int n)
+{
+	int c;
+
+	for (c = 0; a[c] != '\0'; c++)
+		_putchar(a[c]);
+		if (c == n)
+			break;
+	_putchar('\n');
+}
