7c47ea9c924a04e17adc9fd433167c4ccb9fe5f8
Ngwere <ngwerecliford@gmail.com>
2022-12-22 02:51:03 -0800

    hot fixes

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,47 +1,27 @@
 nclude "main.h"
 
-
-
 /**
-
- * rot13 - encodes a string into rot13
-
+ * *rot13 - encodes a string into rot13
  * @s: string to encode
-
- *
-
  * Return: address of s
-
  */
 
 char *rot13(char *s)
-
 {
 
 	int i, j;
 
 	char a[] = "aAbBcCdDeEfFgGhHiIjJkKlLmMnNoOpPqQrRsStTuUvVwWxXyYzZ";
-
 	char b[] = "nNoOpPqQrRsStTuUvVwWxXyYzZaAbBcCdDeEfFgGhHiIjJkKlLmM";
 
-
-
 	for (i = 0; *(s + i); i++)
-
 	{
-
 		for (j = 0; j < 52; j++)
-
 		{
-
 			if (a[j] == *(s + i))
-
 			{
-
 				*(s + i) = b[j];
-
 				break;
-
 			}
 
 		}
