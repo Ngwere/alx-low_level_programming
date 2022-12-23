5b7fae9751e817bbb66af786910cb660d1c93cab
Ngwere <ngwerecliford@gmail.com>
2022-12-23 06:26:16 -0800

    created 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _abs - make number positive
+ * @n: The integer to be computed
+ * Return: The abs value of the integer
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+		return (n);
+	else
+		return (-n);
+}
