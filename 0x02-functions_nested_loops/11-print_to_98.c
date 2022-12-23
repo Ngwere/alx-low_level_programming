d2977ac3bbd683c22c1ea31713c14e9773c09adf
Ngwere <ngwerecliford@gmail.com>
2022-12-23 07:39:16 -0800

    added task 11

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * print_to_98 - print numbers upto 98
+ * @n: the number
+ * Return: void
+ */
+void print_to_98(int n)
+{
+	for (int i = n; i < 98; i++)
+		printf("%d, ", i);
+	printf("%d", i);
+}
