5e9df746896b581ede29636599f4d27747b308df
Ngwere <ngwerecliford@gmail.com>
2022-12-24 10:30:37 -0800

    Optial errors check

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,11 +10,11 @@ int main(void)
 
 	for (i = 0; i < 9; i++)
 	{
-		for (n = i+1; n <= 9; n++)
+		for (n = i + 1; n <= 9; n++)
 		{
 			putchar((i % 10) + '0');
 			putchar((n % 10) + '0');
-			if  (n != 9)
+			if (i != 8)
 				putchar(',');
 			putchar(' ');
 		}
