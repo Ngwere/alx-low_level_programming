be84da13c4b04fe25e15ee8575d29e61a57b7c2d
Ngwere <ngwerecliford@gmail.com>
2022-12-24 10:13:36 -0800

    Optional task 1

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - main entry
+ * Return: 0 always
+ */
+int main(void)
+{
+	int i,n;
+
+	for (i = 0; i < 9; i++)
+	{
+		for (n = i+1; n <= 9; n++)
+		{
+			putchar((i % 10) + '0');
+			putchar((n % 10) + '0');
+			if  (n != 9)
+				putchar(',');
+			putchar(' ');
+		}
+	}
+	return (0);
+}
+			
+
