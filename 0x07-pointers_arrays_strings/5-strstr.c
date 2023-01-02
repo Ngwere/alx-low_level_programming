049a884bac0504fa8571b56a5368586c982452f1
Ngwere <ngwerecliford@gmail.com>
2023-01-02 12:38:16 -0800

    Created optional task 1

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -13,11 +13,9 @@ char *_strstr(char *haystack, char *needle)
 	unsigned int i = 0, j = 0;
 
 	while (haystack[i])
-
 	{
 
 		while (needle[j] && (haystack[i] == needle[0]))
-
 		{
 
 			if (haystack[i + j] == needle[j])
@@ -31,7 +29,6 @@ char *_strstr(char *haystack, char *needle)
 		}
 
 		if (needle[j])
-
 		{
 
 			i++;
