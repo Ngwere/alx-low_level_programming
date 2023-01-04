3eb544d6ebb16468dcd0764d2c9f344804e6d36d
Ngwere <ngwerecliford@gmail.com>
2023-01-04 21:32:35 +0000

    task 1 created

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,23 +1,15 @@
 #include "main.h"
-
 /**
- *  _print_rev_recursion - print a string by reverse
- *
- * @s: string to print
- *
- * Return: void
- */
+ * _print_rev_recursion - print a string in reverse
+ * @s: string
+ * Return 0;
+*/
+
 void _print_rev_recursion(char *s)
 {
-	int i;
-
 	if (*s)
 	{
 		_print_rev_recursion(s + 1);
 		_putchar(*s);
-	}
-	else
-	{
-		_putchar('\n');
 	}
 }
