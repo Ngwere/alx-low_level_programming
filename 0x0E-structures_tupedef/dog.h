9713328ecb923a4cbc6f7b00c9c9ccd1a6dcc96b
Ngwere <ngwerecliford@gmail.com>
2023-01-17 03:25:39 +0000

    hot fixes

--- 0x0E-structures_tupedef/dog.h
+++ 0x0E-structures_tupedef/dog.h
@@ -1,17 +1,30 @@
-#ifdef DOG
+#ifndef DOG
 #define DOG
 
 /**
- * struct dog - dog class 
- * @name: Name of the dog 
- * @age: Age of the dog 
+ * struct dog - dog class
+ *
+ * @name: Name of the dog
+ *
+ * @age: Age of the dog
+ *
  * @owner: Owner of the dog
  */
 
-struct dog {
-	name char; 
-	age float; 
-	owner char;
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
 };
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+
+typedef struct dog dog_t;
+
+dog_t *new_dog(char *name, float age, char *owner);
+
+void free_dog(dog_t *d);
 
 #endif
