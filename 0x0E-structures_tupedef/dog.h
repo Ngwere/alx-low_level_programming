0945567e9ed522e06b350968c8395ed7eea0e55f
Ngwere <ngwerecliford@gmail.com>
2023-01-17 03:23:15 +0000

    hot fix

--- 0x0E-structures_tupedef/dog.h
+++ 0x0E-structures_tupedef/dog.h
@@ -1,6 +1,13 @@
 #ifdef DOG
 #define DOG
 
+/**
+ * struct dog - dog class 
+ * @name: Name of the dog 
+ * @age: Age of the dog 
+ * @owner: Owner of the dog
+ */
+
 struct dog {
 	name char; 
 	age float; 
