fb74bc5703798142e4015035cb58ff9a84361f60
Ngwere <ngwerecliford@gmail.com>
2023-03-25 21:55:24 +0000

    list length

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+#include "lists.h"
+
+/**
+ * print_dlistint - Prints all the elements of a dlistint_t list.
+ * @h: The head of the dlistint_t lis.
+ *
+ * Return: The number of nodes in the list.
+ */
+
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t nodes = 0;
+
+	while (h)
+	{
+		nodes++;
+		h = h->next;
+	}
+
+	return (nodes);
+}
